fc50b5feac57fb1b41c2317cc4ef2931d474cf3c
Arcyvilk <blazemakerrr@gmail.com>
2017-06-06 23:28:53 +0200

    Moved banlist to separate file

---
+++ classes/ban.js
@@ -0,0 +1,61 @@
+﻿const Discord = require('discord.js');
+var Roles = require('./roles.js');
+
+exports.Ban = function (data) {
+    var ban = this;
+
+    ban.newUserIsBlacklisted = function (GuildMember) {
+        for (i in ban.blacklist) {
+            if (GuildMember.user.username.toLowerCase() == ban.blacklist[i][0].toLowerCase()
+                && GuildMember.user.discriminator == ban.blacklist[i][1]) {
+                console.log(`there he is`);
+                return true;
+            }
+        }
+        return false;
+    };
+    ban.handleUserFromBlacklist = function (GuildMember) {
+        var roles = new Roles.Roles(GuildMember);
+        roles.addRoleToUser(`timeout`);
+        GuildMember.user.send(`<:lazored:288786608952442881>`);
+
+        var embed = new Discord.RichEmbed()
+            .setTitle(`USER FROM BLACK LIST JOINS`)
+            .setColor(0xB83FE0)
+            .setDescription(`**User:** \`\`${GuildMember.user.username}#${GuildMember.user.discriminator}\`\`` +
+            `\n**Timestamp:**\`\`${GuildMember.joinedAt}\`\``)
+        data.bot.channels.get(data.logChannel).send({ embed });
+        data.bot.channels.get(data.logChannel).send(`<@&207733800996831242> - user ${GuildMember.user.username}#${GuildMember.user.discriminator} got preemptively given a timeout role.`);
+        return;
+    };
+
+    ban.blacklist = [
+        [`Poro`, `0069`],
+        [`Son`, `9765`],
+        [`Fading`, `1461`],
+        [`Kappacino`, `1440`],
+        [`DidYouEatCrab`, `4893`],
+        [`hothole`, `6007`],
+        [`Dark Phoenix`, `4156`],
+        [`dandeedandee`, `5917`],
+        [`Snivel`, `4800`],
+        [`Kronus`, `6870`],
+        [`Shindae`, `3005`],
+        [`Jesias`, `6016`],
+        [`Ønly Rengar`, `7841`],
+        [`Blue`, `2926`],
+        [`Froze`, `0864`],
+        [`Onee-chan~`, `6102`],
+        [`cancer op`, `6587`],
+        [`SlippB`, `1382`],
+        [`Jesias`, `6016`],
+        [`macc`, `0552`],
+        [`Saware`, `4087`],
+        [`infamous steady`, `9391`],
+        [`206089`, `2511`],
+        [`Turbovirginschwarzkopf`, `1400`],
+        [`➣ 𝕲𝖆𝖇𝖗𝖎𝔢𝖑`, `7001`],
+        [`Zenod`, `0071`],
+        [`TisClobberinTime`, `7076`],
+        [`Based Vassilij`, `0627`]];
+};
--- viktor.js
+++ viktor.js
@@ -6,6 +6,7 @@ var Data = require('./classes/data.js');
 
 var Roles = require('./classes/roles.js');
 var Stream = require('./classes/stream.js');
+var Ban = require('./classes/ban.js');
 
 const bot = new Discord.Client();
 const token = process.env.API_KEY;
@@ -100,16 +101,13 @@ bot.on('guildMemberAdd', GuildMember => {
     try { data.whatServer(GuildMember.guild.id); }
     catch (err) { }//this triggers when message was sent in DM
 
+    console.log(`${GuildMember.user.username}#${GuildMember.user.discriminator}`);
+
     if (data.server == `vikmains`) {
-        for (i in listOfDravenUsernamesAndDiscriminators)
-        {
-            if (GuildMember.user.username == listOfDravenUsernamesAndDiscriminators[i][0] && GuildMember.user.discriminator == listOfDravenUsernamesAndDiscriminators[i][0]) {
-                var roles = new Roles.Roles(GuildMember);
-                roles.addRoleToUser(`timeout`);
-                GuildMember.user.send(`Lol. Nope. <:lazored:288786608952442881>`);
-            }
-        }
-        GuildMember.user.send(data.welcomeMessageForViktorMains);
+        var ban = new Ban.Ban(data);
+        if (ban.newUserIsBlacklisted(GuildMember))
+            ban.handleUserFromBlacklist(GuildMember);
+        else GuildMember.user.send(data.welcomeMessageForViktorMains);
     }
 
     var embed = new Discord.RichEmbed()
@@ -156,32 +154,3 @@ bot.on('presenceUpdate', (oldMember, newMember) => {
         console.log(`${err} while removing streaming role from ${newMember.user.username}`);
     }
 });
-
-var listOfDravenUsernamesAndDiscriminators = [[`Poro`,`0069`], 
-[`Son`,`9765`],
-[`Fading`,`1461`],
-[`Kappacino`,`1440`],
-[`DidYouEatCrab`,`4893`],
-[`hothole`,`6007`],
-[`Dark Phoenix`,`4156`],
-[`dandeedandee`,`5917`],
-[`Snivel`,`4800`],
-[`Kronus`,`6870`],
-[`Shindae`,`3005`],
-[`Jesias`,`6016`],
-[`�nly Rengar`,`7841`],
-[`Blue`,`2926`],
-[`Froze`,`0864`],
-[`Onee-chan~`,`6102`],
-[`cancer op`, `6587`],
-[`SlippB`,`1382`],
-[`Jesias`,`6016`],
-[`macc`,`0552`],
-[`Saware`,`4087`], 
-[`infamous steady`,`9391`],
-[`206089`,`2511`],
-[`Turbovirginschwarzkopf`,`1400`],
-[`? ??????????????`,`7001`],
-[`Zenod`, `0071`],
-[`TisClobberinTime`, `7076`],
-[`Feuer`,`6277`]];
