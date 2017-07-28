f3fa4eabae0ae6f244f40738ecd4049bdc73b027
Arcyvilk <blazemakerrr@gmail.com>
2017-07-28 21:07:58 +0200

    Can now ban guild members via ID

--- classes/mod/ban.js
+++ classes/mod/ban.js
@@ -31,13 +31,14 @@
     };
 
 
-    ban.ban = function () { //remake it so half of code doesn't repeat itself
+    ban.ban = function () { 
         var d = new Date();
         var banOptions = input.removeKeyword(data.message.content).split('|');
+        var idToBan = banOptions[0];
 
         if (banOptions[1] == undefined)
             banOptions[1] == `No reason specified.`;
-        if (data.message.mentions.users.first()) {
+        if (data.message.mentions.users.first()) { //if banned by mention
             data.message.guild.ban(data.message.mentions.users.first().id)
                 .then(user => {
                     post.embedToChannel(`<:banhammer:310437806772060168> MEMBER BANNED`, [
@@ -49,14 +50,35 @@
                     ], data.logChannel, `F27900`);
                     return post.embed(`:white_check_mark: Success!`, [[`___`, `${banOptions[0]} banned! Hopefully they deserved it.`, false]]);
                 })
-                .catch(error => { 
-                    return post.embed(`:warning: Error while banning user`,[[`___`,`${error}`,false]]);
+                .catch(error => {
+                    return post.embed(`:warning: Error while banning user`, [[`___`, `${error}`, false]]);
                 });
-        };
+        }
+        else { //if banned by ID & is on the server
+            var userIsOnThisServer = data.message.guild.members.get(idToBan);
+            if (userIsOnThisServer) {
+                data.message.guild.ban(idToBan)
+                    .then(user => {
+                        post.embedToChannel(`<:banhammer:310437806772060168> MEMBER BANNED`, [
+                            [`User banned`, `${userIsOnThisServer.user.username}\n(${idToBan})`, true],
+                            [`Banned by`, data.message.author.username, true],
+                            [`Reason`, banOptions[1], false],
+                            [`Date`, d, false],
+                        ], data.logChannel, `F27900`);
+                        return post.embed(`:white_check_mark: Success!`, [[`___`, `${userIsOnThisServer.user.username} banned! Hopefully they deserved it.`, false]]);
+                    })
+                    .catch(error => {
+                        return post.embed(`:warning: Error while banning user`, [[`___`, `${error}`, false]]);
+                    });
+            }
+            else {
+                //add to blacklist and wait for him to come >:3c
+                //fs.readFile(banPath, 'utf8', (err, fileContents) => {
 
-        fs.readFile(banPath, 'utf8', (err, fileContents) => {
-
-        });
+                //});
+                post.message(`This person is not on server and will get blacklisted instead of banned`);
+            }
+        }
     };
     ban.unban = function () {
         var d = new Date();
@@ -74,14 +96,43 @@
                                 //  [`Reason for original ban`, wasOnBanList.reason, false],
                                 [`Date`, d, false],
                             ], data.logChannel, `FF0088`);
-                            return post.embed(`:white_check_mark: Success!`, [[`___`,`${idToUnban} unbanned!`,false]]);
+                            return post.embed(`:white_check_mark: Success!`, [[`___`, `${idToUnban} unbanned!`, false]]);
                         })
                         .catch(error => {
                             return post.embed(`:warning: Error while unbanning user`, [[`___`, `${error}`, false]]);
                         });
                 }
-                else
-                    return post.embed(`:warning: Error while unbanning user`, [[`___`, `Can't unban user who ain't banned yet.`, false]]);
+                else {
+                    fs.readFile(banPath, 'utf8', (err, blackList) => {
+                        if (err)
+                            return post.embed(`:warning: Error while unbanning user`, [[`___`, `${err}`, false]]);
+                        var serverID = data.message.guild.id;
+                        blackList = JSON.parse(blackList);
+
+                        if (blackList.Blacklist.hasOwnProperty(serverID)) {
+                            for (i in blackList.Blacklist[serverID].bans) {
+                                if (blackList.Blacklist[serverID].bans[i].id == idToUnban) {
+                                    var reason = blackList.Blacklist[serverID].bans[i].reason;
+                                    blackList.Blacklist[serverID].bans.splice(i, 1);
+                                    fs.writeFile(banPath, JSON.stringify(blackList), err => {
+                                        if (err)
+                                            return post.embed(`:warning: Error while unbanning user`, [[`___`, `${error}`, false]]);
+                                        post.embedToChannel(`<:banhammer:310437806772060168> MEMBER REMOVED FROM BLACKLIST`, [
+                                            [`User`, `${idToUnban})`, true],
+                                            [`Unbanned by`, data.message.author.username, true],
+                                            [`Reason for original ban`, reason, false],
+                                            [`Date`, d, false],
+                                        ], data.logChannel, `FF0088`);
+                                        return post.embed(`:white_check_mark: Success!`, [[`___`, `${idToUnban} removed from blacklist!`, false]]);
+                                    });
+                                }
+                            }
+                            return post.embed(`:warning: Error while unbanning user`, [[`___`, `Can't unban user who ain't banned yet.`, false]]);
+                        }
+                        else
+                            return post.embed(`:warning: Error while unbanning user`, [[`___`, `Can't unban user who ain't banned yet.`, false]]);
+                    });
+                }
             })
             .catch(error => {
                 return post.embed(`:warning: Error while unbanning user`, [[`___`, `${error}`, false]]);
