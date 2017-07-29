b9ec1a6841817abd2f94826476b2b6796086dfb9
Arcyvilk <blazemakerrr@gmail.com>
2017-07-29 11:49:57 +0200

    Bot now teaches people how to assign rank role

--- classes/roles.js
+++ classes/roles.js
@@ -24,6 +24,14 @@ exports.Roles = function (member) {
             return true;
         return false;
     };
+    roles.roleIsRankRole = function (roleName) {
+        var arrayOfRanks = [`challenger`, `master`, `diamond`, `platinum`, `gold`, `silver`, `bronze`];
+        for (i in arrayOfRanks) {
+            if (roleName.toLowerCase() == arrayOfRanks[i])
+                return true;
+        };
+        return false;
+    };
 
     roles.addRoleToUser = function (roleName) {
         if (roles.requestedManually && !roles.allRequirementsMet('add', roleName,
@@ -85,6 +93,11 @@ exports.Roles = function (member) {
         }
         if (roles.jokeRoleRequested(roleName) && roleAction==`add`)
             return post.message(`Indeed. You are.`);
+        if (roles.roleIsRankRole(roleName) && roleAction==`add`)
+            return post.embed(`:information_source: This is not how rank roles are assigned`, [[`___`, `Rank roles are assigned manually by moderators.\n\n` +
+                `1. **Screenshot your profile** with nickname and rank badge visible, like that: http://i.imgur.com/aiRJudZ.png \n` +
+                `2. Post the screenshot in the #assign_role room. \n\n` +
+                `The colour will be based off *Ranked Solo/Duo* - Ranked Flex and Ranked 3v3 aren't taken into account.`, false]]);
         if (!roles.roleExists(roleName)) {
             post.message(`Such role doesn't exist.`);
             return false;
