42b044bde0d0b03e4322c4f6a53d5efbb99a4c6a
Arcyvilk <blazemakerrr@gmail.com>
2018-02-10 12:04:33 +0100

    Bugfix of emoji reaction

--- classes/answer.js
+++ classes/answer.js
@@ -158,7 +158,7 @@ exports.Answer = function (data) {
         if (commands.listOfResponses.hasOwnProperty(keyword))
             answer.checkForModPrivileges(commands.listOfResponses[keyword]);
         else
-            data.message.react('<:questionmark:244535324737273857>');
+            data.message.react(':questionmark:244535324737273857');
     };
     answer.toDearViktor = function () {
         var msg = answer.userMessage.content.toLowerCase();
