bab5a93fc15ce10ced2f9b3d2f830a01da8206fe
Arcyvilk <blazemakerrr@gmail.com>
2019-04-16 22:48:52 +0200

    :sparkles: reintroduced commands and keywords logic

--- viktor.js
+++ viktor.js
@@ -1,10 +1,12 @@
 import Discord from 'discord.js';
 import config from './config.json';
 import { log } from './log';
+import { message } from './lib/message';
 
 const bot = new Discord.Client();
 
 bot.on('ready', () => log.INFO('Great Herald started working!'));
+bot.on('message', message.classify);
 
 
 bot.login(config.DISCORD_TOKEN);
