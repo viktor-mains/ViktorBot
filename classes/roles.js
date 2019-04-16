4a668349190c7c49d836cc7b20aaacf2f2bcf548
A.G <a.gadacz@gmail.com>
2019-04-16 17:59:31 +0200

    :sparks: some roles arenow blocked from self-assigning (#9)

--- classes/roles.js
+++ classes/roles.js
@@ -73,21 +73,29 @@ exports.Roles = function (member) {
         return false;
     };
     roles.rankRoleRequested = function (roleName) {
-        var arrayOfRanks = [`challenger`, `master`, `diamond`, `platinum`, `gold`, `silver`, `bronze`];
+        var arrayOfRanks = [`challenger`, `grandmaster`, `master`, `diamond`, `platinum`, `gold`, `silver`, `bronze`, `iron`];
         for (i in arrayOfRanks) {
             if (roleName.toLowerCase() == arrayOfRanks[i])
                 return true;
         };
         return false;
     };
-    roles.userRankRoleRequested = function (roleName) {
-        var arrayOfUserRanks = [`Fresh Acolyte`, `Junior Assistant`, `Hextech Progenitor`, `Arcane Android`];
+    roles.userSpecialRoleRequested = function (roleName) {
+        var arrayOfUserRanks = [`Fresh Acolyte`, `Junior Assistant`, `Hextech Progenitor`, `Arcane Android`, `Artist`, `Winner of the 1v1 Tournament`];
         for (i in arrayOfUserRanks) {
             if (roleName.toLowerCase() == arrayOfUserRanks[i].toLowerCase())
                 return true;
         };
         return false;
     };
+    roles.modRoleRequested = function (roleName) {
+        var arrayOfUserRanks = [`Superior Construct`, `Admin`, `Administrator`, `Administrators`, `Mod`, `Moderator`, `Moderators`, `Experimental AI`, `Reddit Moderator`, `Reddit Moderators`];
+        for (i in arrayOfUserRanks) {
+            if (roleName.toLowerCase() == arrayOfUserRanks[i].toLowerCase())
+                return true;
+        };
+        return false;
+    }
 
     roles.allRequirementsMet = function (roleAction, roleName, wrongChatRoom, userHasOrHasNotRole) {
         var post = new Post.Post(roles.data);
@@ -103,8 +111,10 @@ exports.Roles = function (member) {
                 `1. **Screenshot your profile** with nickname and rank badge visible, like that: http://i.imgur.com/aiRJudZ.png \n` +
                 `2. Post the screenshot in the #bot_commands room. \n\n` +
                 `The colour will be based off *Ranked Solo/Duo* - Ranked Flex and Ranked 3v3 aren't taken into account.`, false]]);
-        if (roles.userRankRoleRequested(roleName))
+        if (roles.userSpecialRoleRequested(roleName))
             return post.message(`:warning: This role can't be added neither removed manually.`);
+        if (roles.modRoleRequested(roleName))
+            return post.message(`:warning: Heh. You'd like to. But nope.`);
         if (!roles.roleExists(roleName)) {
             post.message(`Such role doesn't exist.`);
             return false;
