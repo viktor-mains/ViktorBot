b5d98d528b8f38c08ad91e54c8b678b52cf20091
Anna Gadacz <anna.gadacz@relayr.io>
2020-01-07 18:13:28 +0100

    :sparkles: iam and iamnot commands

---
+++ lib/commands/commands/roles.ts
@@ -0,0 +1,91 @@
+import Discord from 'discord.js';
+import { log } from '../../log';
+import { removeKeyword, extractArguments } from '../../helpers';
+import { updateCache } from '../../storage/db';
+import { cache } from '../../storage/cache';
+import config from '../../../config.json';
+
+const returnRoleID = (roleName, member) => {
+    const role = member.guild.roles.find(role => role.name.toLowerCase() === roleName.toLowerCase()).id;
+    return role;
+};
+const roleExists = (roleName, member) => {
+    return member.guild.roles.some(role => role.name.toLowerCase() === roleName.toLowerCase());
+};
+const roleisAssignable = (roleName) => {
+    const assignableRoles = cache["options"].find(option => option.option === 'assignableRoles')
+        ? cache["options"].find(option => option.option === 'assignableRoles').value
+        : null;
+    if (!assignableRoles || !assignableRoles.find(role => role.toLowerCase() === roleName.toLowerCase()))
+        return false;
+    return true;
+}
+const userHasRole = (roleName, member) => {
+    if (member.roles.has(returnRoleID(roleName, member)))
+        return true;
+    return false;
+};
+const requestWasSendInApropriateChannel = (msg:Discord.Message) => {
+    const room_roles = cache["options"].find(option => option.option === 'room_roles')
+        ? cache["options"].find(option => option.option === 'room_roles').value
+        : null;
+    if (room_roles == msg.channel.id)
+        return true;
+    return false;
+};
+
+export const iam = (msg:Discord.Message) => {
+    const roleName = extractArguments(msg)[0];
+    const member = msg.member;
+    
+    if (!roleName)
+        return msg.channel.send(`Excuse me, you are _what?_`);
+    if (!roleExists(roleName, member))
+        return msg.channel.send(`Role **[${roleName.toUpperCase()}]** doesn't exist.`);
+    if (!roleisAssignable(roleName))
+        return msg.channel.send(`Role **[${roleName.toUpperCase()}]** cannot be self-assigned.`);
+    if (userHasRole(roleName, member))
+        return msg.channel.send(`You already have the **[${roleName.toUpperCase()}]** role.`);
+    if (!requestWasSendInApropriateChannel(msg))
+        return msg.channel.send(`You can be anything you want, I'm not giving you role outside the <#${cache["options"].find(option => option.option === 'room_roles').value}> room.`);
+
+    msg.member.addRole(returnRoleID(roleName, member))
+        .then(success => msg.channel.send(`Role **[${roleName.toUpperCase()}]** assigned to ${member.user.username} with utmost efficiency.`))
+        .catch(error => {
+            msg.channel.send(`Failed to assign the **[${roleName.toUpperCase()}]** role.`);
+            console.log(error);
+        });
+    return;
+}
+export const iamnot = (msg:Discord.Message) => {
+    const roleName = extractArguments(msg)[0];
+    const member = msg.member;
+    
+    if (!roleName)
+        return msg.channel.send(`Excuse me, you aren't _what?_`);
+    if (!roleExists(roleName, member))
+        return msg.channel.send(`Role **[${roleName.toUpperCase()}]** doesn't exist.`);
+    if (!roleisAssignable(roleName))
+        return msg.channel.send(`Role **[${roleName.toUpperCase()}]** cannot be self-unassigned.`);
+    if (!userHasRole(roleName, member))
+        return msg.channel.send(`You don't have the **[${roleName.toUpperCase()}]** role.`);
+    if (!requestWasSendInApropriateChannel(msg))
+        return msg.channel.send(`I'm not doing that outside the <#${cache["options"].find(option => option.option === 'room_roles').value}> room.`);
+
+    msg.member.removeRole(returnRoleID(roleName, member))
+        .then(success => msg.channel.send(`Role **[${roleName.toUpperCase()}]** removed from ${member.user.username} with utmost efficiency.`))
+        .catch(error => {
+            msg.channel.send(`Failed to remove the **[${roleName.toUpperCase()}]** role.`);
+            console.log(error);
+        });
+    return;
+}
+export const roles = (msg:Discord.Message) => {
+    msg.channel.send('WIP')
+}
+export const membership = (msg:Discord.Message) => {
+    msg.channel.send('WIP')
+}
+export const topmembers = (msg:Discord.Message) => {
+    msg.channel.send('WIP')
+}
--- lib/commands/list.ts
+++ lib/commands/list.ts
@@ -10,24 +10,35 @@ import {
 import { help, hmod, opgg } from './commands/basic';
 import { } from './commands/faq';
 import { meow, woof, rito, choose } from './commands/fun';
-import { status, impersonate } from './commands/mod';
+import { status, impersonate, refresh } from './commands/mod';
 import { updatechampions, lastlane } from './commands/riot';
+import { iam, iamnot, roles, membership, topmembers } from './commands/roles';
 
 export const Command: { [key:string]: (command:ICommand, msg:Discord.Message) => string | void} = {
     help: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(help, msg),
     hmod: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(hmod, msg),
+    opgg: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(opgg, msg),
+
+    meow: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(meow, msg),
+    woof: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(woof, msg),
+    rito: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(rito, msg),
+    choose: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(choose, msg),
     beep: (command:ICommand, msg:Discord.Message) => new TextCommand(command, msg).execute('_sighs deeply_\nBeep. Boop.'),
     clubs: (command:ICommand, msg:Discord.Message) => new TextCommand(command, msg).execute('List of in-game clubs we know about: <https://www.reddit.com/r/viktormains/wiki/clubs>'),
     faq: (command:ICommand, msg:Discord.Message) => new TextCommand(command, msg).execute('Useful tips and tricks for new Viktor players: <https://www.reddit.com/r/viktormains/wiki/faq>'),
-    gibeskin: (command:ICommand, msg:Discord.Message) => new TextCommand(command, msg).execute('http://arcyvilk.com/greatherald/img/gibeskin.png'),
     joke: (command:ICommand, msg:Discord.Message) => new TextCommand(command, msg).execute('I won\'t waste my precious time for sake of your personal amusement.'),
-    rito: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(rito, msg),
+    gibeskin: (command:ICommand, msg:Discord.Message) => new TextCommand(command, msg).execute('http://arcyvilk.com/greatherald/img/gibeskin.png'),
+    
     status: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(status, msg),
-    meow: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(meow, msg),
-    woof: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(woof, msg),
-    choose: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(choose, msg),
-    opgg: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(opgg, msg),
     impersonate: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(impersonate, msg),
+    refresh: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(refresh, msg),    
+    
     updatechampions: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(updatechampions, msg),
     lastlane: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(lastlane, msg),
+
+    iam: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(iam, msg),
+    iamnot: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(iamnot, msg),
+    roles: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(roles, msg),
+    membership: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(membership, msg),
+    topmembers: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(topmembers, msg),
 };
