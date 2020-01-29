5b306bbcc159e35dac13d2681b8788dbc66f00af
Arcyvilk <blazemakerrr@gmail.com>
2020-01-29 22:25:29 +0100

    :tada: finalizing!

--- viktor.js
+++ viktor.js
@@ -4,7 +4,7 @@ import config from './config.json';
 import { log } from './lib/log';
 import { classifyMessage } from './lib/message';
 import { connectToDb } from './lib/storage/db';
-import { msgEdit, msgDelete, userJoin, userLeave } from './lib/events';
+import { msgEdit, msgDelete, userJoin, userLeave, botJoin } from './lib/events';
 import { cache } from './lib/storage/cache';
 
 const bot = new Discord.Client();
@@ -17,22 +17,16 @@ const ready = bot => {
 
 const init = bot => {
     log.INFO('Great Herald started working!');
-    bot.on('guildCreate', guild => log.INFO(`Great Herald joined ${guild.id} guild!`))
     bot.on('message', classifyMessage);
-    bot.on('join', addNewMember);
-
-    // [ TODO ]
-    // bot.on('presenceUpdate', handleStream);
     bot.on('messageUpdate', msgEdit);
     bot.on('messageDelete', msgDelete);
+    bot.on('guildCreate', botJoin)
     bot.on('guildMemberAdd', userJoin);
     bot.on('guildMemberRemove', userLeave);
-}
 
+    // [TODO]
+    // bot.on('presenceUpdate', handleStream);
+}
 
 bot.on('ready', () => ready(bot));
 bot.login(config.DISCORD_TOKEN);
-
-const addNewMember = () => {
-
-}
