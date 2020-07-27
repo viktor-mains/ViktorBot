04cb6fa9fcc522e914e3c846ee1fb50f6c049d06
Dan Pantry <devnull@danpantry.me>
2020-07-27 20:30:42 +0100

    oops

--- viktor.ts
+++ viktor.ts
@@ -1,25 +1,10 @@
-import Discord from "discord.js";
 import config from "./config.json";
-import { log } from "./lib/log";
-import { classifyMessage } from "./lib/message";
 import { connectToDb } from "./lib/storage/db";
-import { msgEdit, msgDelete, userJoin, userLeave, botJoin } from "./lib/events";
+import { initialize } from "./lib/bot";
 
 async function main() {
   await connectToDb(config.DATABASE_URL);
-
-  const bot = new Discord.Client();
-  bot.on("message", classifyMessage);
-  bot.on("messageUpdate", msgEdit);
-  bot.on("messageDelete", msgDelete);
-  bot.on("guildCreate", botJoin);
-  bot.on("guildMemberAdd", userJoin);
-  bot.on("guildMemberRemove", userLeave);
-  bot.on("ready", () => {
-    log.INFO("Great Herald started working!");
-  });
-
-  await bot.login(config.DISCORD_TOKEN);
+  await initialize(config.DISCORD_TOKEN);
 }
 
 main().catch((e) => console.error(e));
