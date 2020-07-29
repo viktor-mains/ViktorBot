a7700c6336860bde90a39bf7ba33745a0827af6f
Arcyvilk <blazemakerrr@gmail.com>
2020-07-29 18:53:38 +0200

    test

--- viktor.ts
+++ viktor.ts
@@ -5,6 +5,9 @@ import { initialize } from './lib/bot';
 async function main() {
 	await connectToDb(config.DATABASE_URL);
 	await initialize(config.DISCORD_TOKEN);
+	const test1 = 1;
+	const test2 = 2;
+	if (test1 == test2) return;
 }
 
 main().catch(e => console.error(e));
